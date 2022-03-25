0ee505ba52f50c61b703d3774478787feb987fd0
Michael Santos <mjs8837@rit.edu>
2022-03-24 21:59:38 -0400

    Finished up the simple models homework.

--- server/models/Dog.js
+++ server/models/Dog.js
@@ -1,27 +1,28 @@
 const mongoose = require('mongoose');
 
-let CatModel = {};
+let DogModel = {};
 
-const CatSchema = new mongoose.Schema({
+const DogSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     trim: true,
     unique: true,
   },
-
-  bedsOwned: {
-    type: Number,
-    min: 0,
+  breed: {
+    type: String,
     required: true,
   },
-
+  age: {
+    type: Number,
+    required: true,
+  },
   createdDate: {
     type: Date,
     default: Date.now,
   },
-
 });
-CatModel = mongoose.model('Cat', CatSchema);
 
-module.exports = CatModel;
+DogModel = mongoose.model('Dog', DogSchema);
+
+module.exports = DogModel;
